47c9bfc0faef11312d7b6e59487ee6c032db17f1
GIFT2105 <letuka.gift21@gmail.com>
2025-01-24 20:38:47 +0200

    Construction deployment fix2

--- components/construction.js
+++ components/construction.js
@@ -1,8 +1,8 @@
 
 const Construction = () => {
     return (
-        <div className="construction">
-            <div className="spinner"></div>
+        <div className="bg-black w-screen h-screen flex flex-col justify-center items-center text-white">
+            <div className=""></div>
             <h1>Site Under Maintenance</h1>
             <p>Site will be down until further notice.</p>
         </div>
